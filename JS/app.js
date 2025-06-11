d53a441cdc41c693ffa90cf3f6cf01338311c579
Danareynolds-coding <dana.reynolds@mscoding.org>
2025-06-11 09:08:18 -0500

    needs work

--- JS/app.js
+++ JS/app.js
@@ -1,29 +1,28 @@
 
-  function makePlayerBoard(){
-    let PlayerBoard = document.getElementById(player-board);
-    for (let i=0; i<4; i++){
-    for(let j=0; j<4; j++){
-    const gridItem = document.getElementById("item");
-    gridItem.classList.add('grid-item')
+  function makePlayerBoard( rows, cols){
+    for (let i=0; i<rows; i++){
+    for(let j=0; j<cols; j++){
+    const gridItem = document.createElement('div');
+    gridItem.classList.add('grid-item');
     gridItem.textContent = arrayPlayer[i][j];
     gridContainer.appendChild(gridItem)
-}
-}
+      }
+    }
   }
-  makePlayerBoard()
+  makePlayerBoard(3,3);
 
-  function makeEnemyBoard(){
-    let EnemyBoard = document.getElementById(enemy-board);
-    for (let i=0; i<4; i++){
-    for(let j=0; j<4; j++){
-    const gridItem = document.getElementById("item");
-    gridItem.classList.add('grid-item')
-    gridItem.textContent = arrayPlayer[i][j];
-    gridContainer.appendChild(gridItem)
-}
-}
-  }
-  makeEnemyBoard()
+//   function makeEnemyBoard(){
+//     let EnemyBoard = document.getElementById(enemy-board);
+//     for (let i=0; i<4; i++){
+//     for(let j=0; j<4; j++){
+//     const gridItem = document.createElement("div");
+//     gridItem.classList.add('grid-item')
+//     gridItem.textContent = arrayPlayer[i][j];
+//     gridContainer.appendChild(gridItem)
+// }
+// }
+//   }
+//   makeEnemyBoard()
 
 
   
